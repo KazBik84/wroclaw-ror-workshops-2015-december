2360519867fb46a1a2cf19650aecc03bb711efc3
Kazimierz Bik <kazimierz.bik@gmail.com>
2015-11-22 07:40:36 -0400

    Filled in the avg_notes so it calculate the note values corectly

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,12 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = student.subject_item_notes.where(subject_item_id: subject_item.id)
+    avg_notes = notes.average(:value)
+    if avg_notes == nil
+      return "0.00"
+    else
+      avg = sprintf( "%0.02f", avg_notes)
+    end
   end
 end
