f07cd7526d45c739622d270dd9259afa605a0282
Kazimierz Bik <kazimierz.bik@gmail.com>
2015-12-01 05:52:18 -0400

    added sceanrio Add new one with valid input and birthdate

--- spec/features/students/user_creates_student_spec.rb
+++ spec/features/students/user_creates_student_spec.rb
@@ -8,12 +8,22 @@ feature 'User creates student' do
     click_on 'Add new one'
   end
 
-  scenario 'with valid input' do
+  scenario 'with valid input without birthdate' do
     fill_in 'First name', with: 'Adrian'
     fill_in 'Last name', with: 'Nowacki'
     click_button 'Create Student'
     expect(page).to have_content 'Student has been created!'
   end
+
+  scenario 'with valid input and birthdate' do
+    fill_in 'First name', with: 'Adrian'
+    fill_in 'Last name', with: 'Nowacki'
+    select '1999', from: 'student_birthdate_1i'
+    select 'January', from: 'student_birthdate_2i'
+    select '3', from: 'student_birthdate_3i'
+    click_button 'Create Student'
+    expect(page).to have_content 'Student has been created!'
+  end  
 
   scenario 'with missing first name' do
     fill_in 'Last name', with: 'Nowacki'
