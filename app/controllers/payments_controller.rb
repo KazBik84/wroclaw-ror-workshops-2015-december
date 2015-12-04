e9e1bbd2308c4180395c9cc1e9a81030160b631d
Kazimierz Bik <kazimierz.bik@gmail.com>
2015-12-04 01:14:14 -0400

    Added full index action with view

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -22,11 +22,29 @@ class PaymentsController < ApplicationController
 
   def full_index
     @students = Student.all
+    @january = payment_search(2015, 1)
+    @february = payment_search(2015, 2)
+    @march = payment_search(2015, 3)
+    @april = payment_search(2015, 4)
+    @may = payment_search(2015, 5)
+    @june = payment_search(2015, 6)
+    @july = payment_search(2015, 7)
+    @august = payment_search(2015, 8)
+    @september = payment_search(2015, 9)
+    @october = payment_search(2015, 10)
+    @november = payment_search(2015, 11)
+    @december = payment_search(2015, 12)
+    @months = [ @january, @february, @march, @april, @may, @june,
+                @july, @august, @september, @october, @november, @december ]
   end
 
   private
 
   def payment_params
     params.require(:payment).permit(:date, :amount, :student_id)
+  end
+
+  def payment_search(year, month)
+    Payment.where('extract(year from date) = ?', year).where('extract(month from date) = ?', month).to_a
   end
 end
