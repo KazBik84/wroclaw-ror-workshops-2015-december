92c20147be0725745b785262ffc47243de0d2c99
Kazimierz Bik <kazimierz.bik@gmail.com>
2015-12-05 17:02:02 -0400

    Added test that checks has_many payments

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Payment do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :date }
+    it { is_expected.to validate_presence_of :amount }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column :date }
+    it { should have_db_column :amount }
+    it { should have_db_column :student_id }
+  end  
+
+    describe 'associations' do
+    it { is_expected.to belong_to :student }
+  end
 end
