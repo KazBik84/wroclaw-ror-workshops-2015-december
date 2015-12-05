d02c873d470e4018f984618d6c24430c2d26842e
Kazimierz Bik <kazimierz.bik@gmail.com>
2015-12-05 17:28:00 -0400

    Full feature tests

--- spec/features/students/user_add_payment_spec.rb
+++ spec/features/students/user_add_payment_spec.rb
@@ -6,8 +6,7 @@ feature "User add payment to the student" do
   background do 
     sign_in
     expect(page).to have_content 'Logout'
-    visit students_path
-      puts page.body    
+    visit students_path 
     find(:xpath, "//a[@title='Add payment']").click
   end
 
@@ -17,6 +16,18 @@ feature "User add payment to the student" do
       expect(page).to have_content('RoR Workhops » Students » Krzysztof Jerzyna new payment')
     end
   end
-    
+  scenario 'with valid input' do
+      fill_in 'Amount', with: '100'
+      select '2015', from: 'payment_date_1i'
+      select 'January', from: 'payment_date_2i'
+      select '3', from: 'payment_date_3i'
+      click_button 'Create Payment'
+      expect(page).to have_content 'Payment has been created!'
+  end    
+ 
+  scenario 'without amount' do
+    click_button 'Create Payment'
+    expect(page).to have_content "can't be blank"
+  end
 
 end
